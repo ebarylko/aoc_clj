fc03b29a296d17c65b0634ba8d38a1568fad0c76
EItan Barylko <eitan@barylko.com>
2023-05-21 21:33:35 -0700

    Added files for day ten

---
+++ src/aoc_clj/2022/day10.clj
@@ -0,0 +1,55 @@
+(ns aoc-clj.2022.day10
+  (:require [clojure.string :as s]))
+
+(defn noop?
+  [instr]
+  (= \n (first instr)))
+
+(defn instr-val
+  [instr]
+  (if (noop? instr)
+    1
+    2))
+
+(defn parse-instruction-value
+  "Pre: takes an instuction
+  Post: returns the numerical value of the instuction"
+  [instr]
+  (if (noop? instr)
+    0
+  (let [[_ value] (s/split instr #" ")]
+    (Integer/parseInt value))))
+
+(defn within-time?
+  "Pre: takes an amount of cycles C and an instruction value V
+  Post: returns true if the difference of C and V is non-negative"
+  [cycles instr-val]
+  (>= (- cycles instr-val) 0))
+
+(defn cycles-left?
+  "Pre: takes the amount of cycles left C and the instruction P
+  Post: Returns true if P can be completed within C"
+  [cycles instr]
+(let [instr-value (instr-val instr)]
+  (if (within-time? cycles instr-value)
+    (- cycles instr-value)
+    nil)))
+
+(defn cycles-val
+  "Pre: takes current cycle value, cycles left, and an instruction
+  Post: if there are cycles left to complete instruction, returns updated cycle value and cycles left. Otherwise returns given cycle value and cycles left"
+  [[cyc-val cycles num] instr]
+  (println )
+  (let [instr-cyc-val (if (noop? instr) 1 2)]
+  (if (cycles-left? cycles instr)
+    [(+ cyc-val (parse-instruction-value instr))
+     (cycles-left? cycles instr)
+     (inc num)]
+    [cyc-val cycles (inc num)])))
+
+(defn sum-cycles
+  "Pre: takes a series of instructions and the number of cycles N
+  Post: returns the value of the register at cycle N"
+  [instrucs cycles]
+  (first (reduce cycles-val [1 cycles 1] instrucs)))
+
---
+++ test/aoc_clj/2022/day10_test.clj
@@ -0,0 +1,181 @@
+(ns aoc-clj.2022.day10-test
+  (:require [aoc-clj.2022.day10 :as sut]
+            [clojure.string :as s]
+            [clojure.test :as t]))
+
+
+(def input
+  (->> "resources/2022/day10.txt"
+       slurp
+       s/split-lines
+       ))
+
+(def sample [
+             "addx 15 "
+             "addx -11 "
+             "addx 6"
+             "addx -3"
+             "addx 5"
+             "addx -1"
+             "addx -8"
+             "addx 13"
+             "addx 4"
+             "noop"
+             "addx -1"
+             "addx 5"
+             "addx -1"
+             "addx 5"
+             "addx -1"
+             "addx 5"
+             "addx -1"
+             "addx 5"
+             "addx -1"
+             "addx -35"
+             "addx 1"
+             "addx 24"
+             "addx -19"
+             "addx 1"
+             "addx 16"
+             "addx -11"
+             "noop"
+             "noop"
+             "addx 21"
+             "addx -15"
+             "noop"
+             "noop"
+             "addx -3"
+             "addx 9"
+             "addx 1"
+             "addx -3"
+             "addx 8"
+             "addx 1"
+             "addx 5"
+             "noop"
+             "noop"
+             "noop"
+             "noop"
+             "noop"
+             "addx -36"
+             "noop"
+             "addx 1"
+             "addx 7"
+             "noop"
+             "noop"
+             "noop"
+             "addx 2"
+             "addx 6"
+             "noop"
+             "noop"
+             "noop"
+             "noop"
+             "noop"
+             "addx 1"
+             "noop"
+             "noop"
+             "addx 7"
+             "addx 1"
+             "noop"
+             "addx -13"
+             "addx 13"
+             "addx 7"
+             "noop"
+             "addx 1"
+             "addx -33"
+             "noop"
+             "noop"
+             "noop"
+             "addx 2"
+             "noop"
+             "noop"
+             "noop"
+             "addx 8"
+             "noop"
+             "addx -1"
+             "addx 2"
+             "addx 1"
+             "noop"
+             "addx 17"
+             "addx -9"
+             "addx 1"
+             "addx 1"
+             "addx -3"
+             "addx 11"
+             "noop"
+             "noop"
+             "addx 1"
+             "noop"
+             "addx 1"
+             "noop"
+             "noop"
+             "addx -13"
+             "addx -19"
+             "addx 1"
+             "addx 3"
+             "addx 26"
+             "addx -30"
+             "addx 12"
+             "addx -1"
+             "addx 3"
+             "addx 1"
+             "noop"
+             "noop"
+             "noop"
+             "addx -9"
+             "addx 18"
+             "addx 1"
+             "addx 2"
+             "noop"
+             "noop"
+             "addx 9"
+             "noop"
+             "noop"
+             "noop"
+             "addx -1"
+             "addx 2"
+             "addx -37"
+             "addx 1"
+             "addx 3"
+             "noop"
+             "addx 15"
+             "addx -21"
+             "addx 22"
+             "addx -6"
+             "addx 1"
+             "noop"
+             "addx 2"
+             "addx 1"
+             "noop"
+             "addx -10"
+             "noop"
+             "noop"
+             "addx 20"
+             "addx 1"
+             "addx 2"
+             "addx 2"
+             "addx -6"
+             "addx -11"
+             "noop"
+             "noop"
+             "noop"])
+
+(def ex-step (first sample))
+(def neg-step "addx -11")
+(def noop-step "noop")
+(sut/parse-instruction-value ex-step)
+(sut/parse-instruction-value noop-step)
+(sut/parse-instruction-value neg-step)
+(Integer/parseInt "-2877")
+
+
+(t/deftest cycle-value-test
+  (t/is (= 21 (sut/sum-cycles sample 20)))
+  (t/is (= 19 (sut/sum-cycles sample 60)))
+  (t/is (= 18 (sut/sum-cycles sample 100)))
+  (t/is (= 21 (sut/sum-cycles sample 140)))
+  (t/is (= 16 (sut/sum-cycles sample 180)))
+  (t/is (= 18 (sut/sum-cycles sample 220)))
+  )
+
+(take-while #(sut/cycles-left? 2 %) sample)
+(sut/sum-cycles sample 20)
+(sut/sum-cycles (drop 109 sample) 41)
