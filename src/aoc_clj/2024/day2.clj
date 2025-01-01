2514bb42a3b07291e374dc5ac0f10bf032cd68f7
Eitan Barylko <eitan@barylko.com>
2025-01-01 15:12:12 -0800

    Finished up first part

--- src/aoc_clj/2024/day2.clj
+++ src/aoc_clj/2024/day2.clj
@@ -1,6 +1,26 @@
 (ns aoc-clj.2024.day2)
 
+
+
+(defn increasing-or-decreasing-seq?
+  [coll]
+(or (every? neg? coll) (every? pos? coll)))
+
+(defn levels-differ-gradually?
+  [coll]
+  (every? #(< 0 (abs %) 4) coll))
+
+(def valid-report? (every-pred increasing-or-decreasing-seq? levels-differ-gradually?))
+
+(defn is-safe?
+  [report]
+  (->> report
+       (partition 2 1)
+       (map (partial apply -))
+       valid-report?))
+
 (defn num-of-safe-reports
   [reports]
-
-  )
+  (->> reports
+       (filter is-safe?)
+       count))
--- test/aoc_clj/2024/day2_test.clj
+++ test/aoc_clj/2024/day2_test.clj
@@ -1,6 +1,14 @@
 (ns aoc-clj.2024.day2-test
   (:require [aoc-clj.2024.day2 :as sut]
-            [clojure.test :as t]))
+            [clojure.test :as t]
+            [clojure.string :as s]))
+
+(def input
+  (->> "resources/2024/day2.txt"
+       slurp
+       s/split-lines
+       (map #(format "[%s]" %))
+       (map read-string)))
 
 (def sample
   [[7 6 4 2 1]
@@ -11,4 +19,9 @@
   [1 3 6 7 9]])
 
 (t/deftest num-of-safe-reports
-  (t/is (= 2 (sut/num-of-safe-reports sample))))
+  (t/is (= 2 (sut/num-of-safe-reports sample)))
+  (t/is (= 2 (sut/num-of-safe-reports input))))
+
+(s/split-lines "hi\nwhy")
+(map (partial apply -) (partition 2 1 [1 2 3 4]))
+(< 1 1 4)
